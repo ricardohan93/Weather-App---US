4298d3dc8727cc574f1f21ff7afa432c5a868b0a
Ricardo Han <ricardoshhan@gmail.com>
2018-02-20 20:00:19 -0300

    add actionCreator

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = '0377e46a7d6cbd710c217b8b56558ae9';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
